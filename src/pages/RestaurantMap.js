7fb96b252fb0c085862a54e8c85448b67eccad49
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-06-28 16:52:21 +0300

    Modal added

--- src/pages/RestaurantMap.js
+++ src/pages/RestaurantMap.js
@@ -1,11 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { SafeAreaView, View, Text } from 'react-native'
 import Context from '../context/store'
 import MapView, { Marker } from 'react-native-maps';
+import Modal from 'react-native-modal';
 
 const RestaurantMap = props => {
-
+    const [modalFlag, setModalFlag] = useState(false)
+    const [selectedRestaurant, setSelectedRestaurant] = useState({})
     const { state, dispatch } = useContext(Context)
+
+    function selectRestaurant(res) {
+        handleModal()
+        setSelectedRestaurant(res)
+    }
+
+    const handleModal = () => setModalFlag(!modalFlag)
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -17,16 +26,40 @@ const RestaurantMap = props => {
                     state.restaurantList.map((restaurants, index) => {
                         return (
                             <Marker
+                                key={index}
                                 coordinate={{
                                     latitude: restaurants.lat,
                                     longitude: restaurants.lng
                                 }}
                                 tracksViewChanges={false}
+                                onPress={() => selectRestaurant(restaurants)}
                             />
                         )
                     })
                 }
             </MapView>
+
+            <Modal
+                isVisible={modalFlag}
+                onBackdropPress={handleModal}
+                swipeDirection="down"
+                onSwipeComplete={handleModal}
+                style={{ justifyContent: 'flex-end', margin: 0 }}
+            >
+                <SafeAreaView style={{ backgroundColor: 'white', borderTopRightRadius: 10, borderTopLeftRadius: 10 }}>
+                    <View style={{ backgroundColor: 'white', padding: 10, borderTopRightRadius: 10, borderTopLeftRadius: 10 }}>
+                        <Text style={{ fontWeight: 'bold' }}>{selectedRestaurant.name}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                        <Text>{selectedRestaurant.area}</Text>
+                    </View>
+                </SafeAreaView>
+            </Modal>
+
         </SafeAreaView>
     )
 }
